632b07242634b9635cf3502beb0f0ea57eec7a45
Alan Rafael Vazquez Barrueta <rafael879vazquez@gmai.com>
2026-03-02 21:21:59 -0600

    Actualización de texto: Firma de Alan

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        EJEMPLO HECHO POR ALAN XD
+        Ejemplo hecho por rafa vazz tumh tumh
       </p>
     </>
   )
